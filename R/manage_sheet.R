333ce3120a73a5af102c979b9e41f5df2ff15e37
John Ray <j.logan.ray@gmail.com>
2020-01-05 10:12:27 -0800

    some comments

--- R/manage_sheet.R
+++ R/manage_sheet.R
@@ -2,15 +2,19 @@ library(tidyverse)
 library(googlesheets4)
 library(ggmap)
 
+# This code takes a list of locations and geocodes them
 # You will need a Google API key to run this code.
-# You can tell if you do if running this:
+# You can tell if you do if running this code...
 # has_google_key()
-# evalutes to `TRUE`
+# ...evalutes to `TRUE`
 
+# Check on the status of your geocoding credentials
 getOption("ggmap")
 
+# Load the spreadsheet
 df <- read_sheet("https://docs.google.com/spreadsheets/d/1XZ6n-2_RhQnu_ZwuUSUIhLIezA5y0J9MTPd0qJ1Alc0")
 
+# Get the unique set of locations to geocode
 locs <- df %>%
   select(city) %>%
   distinct() %>%
@@ -18,4 +22,5 @@ locs <- df %>%
 
 df <- left_join(df, locs)
 
+# Update the google sheet with a new geocoded/'full' dataset
 sheets_write(df, ss = "https://docs.google.com/spreadsheets/d/1XZ6n-2_RhQnu_ZwuUSUIhLIezA5y0J9MTPd0qJ1Alc0", sheet = "full")
